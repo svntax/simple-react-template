1192d42cc628dd16684a08568321753bc3cb8394
svntax <endless.answers@gmail.com>
2020-12-24 02:46:57 -0500

    First setup

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+	
+	constructor(props){
+		super(props);
+	}
+	
+	render(){
+		return (
+			<div className="app">
+				<h1>Simple React App</h1>
+			</div>
+		);
+	}
+	
+}
+
+export default App;
